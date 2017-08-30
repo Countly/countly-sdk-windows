93c652642f67c7603d8b34da7f0e578d19b24f79
ArtursK <kadikis.arturs@gmail.com>
2017-08-30 21:54:51 +0300

    [net35, net40, pcl, windows, wp7, wp8] Moving SessionEvent class to the common project.

--- countlyCommon/countlyCommon/Entities/UpdateSession.cs
+++ countlyCommon/countlyCommon/Entities/UpdateSession.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2012, 2013, 2014 Countly
+Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017 Countly
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
