f15ef6364be488bdfe760ec34ac744ea91b90102
ArtursK <kadikis.arturs@gmail.com>
2017-08-30 22:13:21 +0300

    [net35, net40, pcl, windows, wp7, wp8] Moving EndSession class to the common project.

--- countlyCommon/countlyCommon/Entities/EndSession.cs
+++ countlyCommon/countlyCommon/Entities/EndSession.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2012, 2013, 2014 Countly
+Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017 Countly
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -38,8 +38,5 @@ namespace CountlySDK.Entities
         {
             Content = String.Format("/i?app_key={0}&device_id={1}&end_session=1&timestamp={2}", appKey, deviceId, TimeHelper.ToUnixTime(DateTime.Now.ToUniversalTime()));
         }
-
-        public EndSession()
-        { }
     }
 }
