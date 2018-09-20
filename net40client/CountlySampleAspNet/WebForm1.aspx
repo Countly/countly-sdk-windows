622be89cebb87c47f22260c2b2d802b400761574
ArtursK <kadikis.arturs@gmail.com>
2018-09-20 18:26:57 +0300

    [net40] Adding asp.net sample application

---
+++ net40client/CountlySampleAspNet/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CountlySampleAspNet.WebForm1" Async="true"%>
+
+<script runat="server">
+
+   private void sample_1(object sender, EventArgs e)
+   {
+      //Debug.WriteLine("Initializing Countly");
+      string str = eventName.Value;
+      sample_output.InnerHtml = str.ToUpper();
+   }
+</script>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h3> Countly SDK ASP.NET sample </h3>
+      
+      <form runat="server">
+         <input runat="server" id="eventName" type="text" />
+         <input runat="server" id="button1" type="submit" value="Enter..." OnServerClick="sample_1"/>
+         
+         <hr />
+         <h3> Results: </h3>
+         <span runat="server" id="sample_output" />
+      </form>
+</body>
+</html>
---
+++ net40client/CountlySampleAspNet/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using CountlySDK;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountlySampleAspNet
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {        
+        const String serverURL = "http://try.count.ly";//put your server URL here
+        const String appKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//put your server APP key here   
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            (StartCountly()).GetAwaiter().GetResult();
+        }
+
+        public async Task StartCountly()
+        {
+            Debug.WriteLine("Initializing Countly");
+            Countly.IsLoggingEnabled = true;
+            await Countly.StartSession(serverURL, appKey, "1.234", Countly.DeviceIdMethod.multipleFields);       
+            Countly.RecordEvent("SomeKey");
+            Thread.Sleep(2000);
+        }
+
+    }
+}
