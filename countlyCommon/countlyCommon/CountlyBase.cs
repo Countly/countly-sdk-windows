e93d10a354092247dcdccd312a6a1858b5011295
ArtursK <kadikis.arturs@gmail.com>
2018-10-17 16:35:50 +0300

    [common, net35, net40, pcl, windows, uwp] Moving upload functions to common base class. Making internal calls non static. Moving also public calls to create events and record exceptions.

--- countlyCommon/countlyCommon/CountlyBase.cs
+++ countlyCommon/countlyCommon/CountlyBase.cs
@@ -35,6 +35,46 @@ namespace CountlySDK.CountlyCommon
         //if set to true, upload will not happen, but will just return "true"
         //data will still be saved in their respective files
         internal bool deferUpload = false;
+
+        // File that stores events objects
+        protected const string eventsFilename = "events.xml";
+        // File that stores sessions objects
+        protected const string sessionsFilename = "sessions.xml";
+        // File that stores exceptions objects
+        protected const string exceptionsFilename = "exceptions.xml";
+        // File that stores temporary stored unhandled exception objects (currently used only for the windows target)
+        protected const string unhandledExceptionFilename = "unhandled_exceptions.xml";
+        // File that stores user details object
+        protected const string userDetailsFilename = "userdetails.xml";
+
+        // Events queue
+        internal List<CountlyEvent> Events { get; set; }
+
+        // Session queue
+        internal List<SessionEvent> Sessions { get; set; }
+
+        // Exceptions queue
+        internal List<ExceptionEvent> Exceptions { get; set; }
+
+        private static CountlyUserDetails userDetails;
+        // User details info
+        public static CountlyUserDetails UserDetails
+        {
+            get
+            {
+                lock (Countly.Instance.sync)
+                {
+                    if (userDetails == null)
+                    {
+                        userDetails = Storage.Instance.LoadFromFile<CountlyUserDetails>(userDetailsFilename).Result;
+                        if (userDetails == null) { userDetails = new CountlyUserDetails(); }
+                        userDetails.UserDetailsChanged += Countly.Instance.OnUserDetailsChanged;
+                    }
+                }
+                return userDetails;
+            }
+        }
+
         // Used for thread-safe operations
         protected object sync = new object();
 
