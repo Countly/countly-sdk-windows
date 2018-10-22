b6ee6883437617fa0c42250f1c644822048ffc7a
ArtursK <kadikis.arturs@gmail.com>
2018-10-23 02:14:36 +0300

    [common, net35, net40, pcl, windows, uwp, std] Updating included nuget packages.

--- net40client/Countly/Properties/AssemblyInfo.cs
+++ net40client/Countly/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Countly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Countly SDK")]
+[assembly: AssemblyDescription("Countly Windows SDK targeting .net4.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Countly")]
 [assembly: AssemblyProduct("Countly")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Countly 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("18.10.0.0")]
+[assembly: AssemblyFileVersion("18.10.0.0")]
