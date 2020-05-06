38496eec9a3e0e122198643e57054e0aa42c8a77
ArtursK <kadikis.arturs@gmail.com>
2020-05-06 22:31:47 +0300

    [net40] Adding wpf sample app

---
+++ net40client/CountlySampleWPF/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using CountlySDK;
+using CountlySDK.Entities;
+
+namespace CountlySampleWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const String serverURL = "";
+        public const String appKey = "";
+
+        protected async override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Debug.WriteLine("Before init");
+
+            Countly.IsLoggingEnabled = true;
+
+            CountlyConfig countlyConfig = new CountlyConfig();
+            countlyConfig.serverUrl = serverURL;
+            countlyConfig.appKey = appKey;
+            countlyConfig.appVersion = "123";
+
+            await Countly.Instance.Init(countlyConfig);
+
+            Countly.UserDetails.Custom.Add("aaa", "666");
+
+            await Countly.Instance.SessionBegin();
+
+            Debug.WriteLine("After init");
+
+        }
+
+        protected async override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Debug.WriteLine("On Exit");
+
+            await Countly.Instance.SessionEnd();
+        }
+    }
+}
