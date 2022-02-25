20c63ea47a11dbe4602cedea048cc208aa6fdf7a
Zahid Zafar <zahidzafar51@gmail.com>
2022-02-25 17:53:49 +0500

    [SDK 450] Internal event records with record event (#37)
    * Internal event records with record event
    
    * PR Changes
    
    * PR Changes
    
    * PR Changes

--- countlyCommon/countlyCommon/CountlyBase.cs
+++ countlyCommon/countlyCommon/CountlyBase.cs
@@ -368,7 +368,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, 1, null, null, null, false);
+            return Countly.Instance.RecordEventInternal(Key, 1, null, null, null);
         }
 
         /// <summary>
@@ -380,7 +380,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key, int Count)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, Count, null, null, null, false);
+            return Countly.Instance.RecordEventInternal(Key, Count, null, null, null);
         }
 
         /// <summary>
@@ -393,7 +393,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key, int Count, double? Sum)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, Count, Sum, null, null, false);
+            return Countly.Instance.RecordEventInternal(Key, Count, Sum, null, null);
         }
 
         /// <summary>
@@ -406,7 +406,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key, int Count, Segmentation Segmentation)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, Count, null, null, Segmentation, false);
+            return Countly.Instance.RecordEventInternal(Key, Count, null, null, Segmentation);
         }
 
         /// <summary>
@@ -420,7 +420,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key, int Count, double? Sum, Segmentation Segmentation)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, Count, Sum, null, Segmentation, false);
+            return Countly.Instance.RecordEventInternal(Key, Count, Sum, null, Segmentation);
         }
 
         /// <summary>
@@ -435,7 +435,7 @@ namespace CountlySDK.CountlyCommon
         public static Task<bool> RecordEvent(string Key, int Count, double? Sum, double? Duration, Segmentation Segmentation)
         {
             if (!Countly.Instance.IsInitialized()) { throw new InvalidOperationException("SDK must initialized before calling 'RecordEvent'"); }
-            return Countly.Instance.RecordEventInternal(Key, Count, Sum, Duration, Segmentation, false);
+            return Countly.Instance.RecordEventInternal(Key, Count, Sum, Duration, Segmentation);
         }
 
         /// <summary>
@@ -445,13 +445,12 @@ namespace CountlySDK.CountlyCommon
         /// <param name="Count">Count to associate with the event, should be more than zero</param>
         /// <param name="Sum">Sum to associate with the event</param>
         /// <param name="Segmentation">Segmentation object to associate with the event, can be null</param>
-        /// <param name="consentOverride">set by views or other features which record their values by events</param>
         /// <returns>True if event is uploaded successfully, False - queued for delayed upload</returns>
-        protected async Task<bool> RecordEventInternal(string Key, int Count, double? Sum, double? Duration, Segmentation Segmentation, bool consentOverride)
+        protected async Task<bool> RecordEventInternal(string Key, int Count, double? Sum, double? Duration, Segmentation Segmentation)
         {
             UtilityHelper.CountlyLogging("[CountlyBase] Calling 'RecordEvent'");
             if (!Countly.Instance.IsServerURLCorrect(ServerUrl)) { return false; }
-            if (!IsConsentGiven(ConsentFeatures.Events) && !consentOverride) { return true; }
+            if (!CheckConsentOnKey(Key)) { return true; }
 
             TimeInstant timeInstant = timeHelper.GetUniqueInstant();
             CountlyEvent cEvent = new CountlyEvent(Key, Count, Sum, Duration, Segmentation, timeInstant.Timestamp);
@@ -467,6 +466,26 @@ namespace CountlySDK.CountlyCommon
             }
 
             return saveSuccess;
+        }
+
+        private bool CheckConsentOnKey(string key)
+        {
+            if (key.Equals(VIEW_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.Views);
+            } else if (key.Equals(NPS_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.Feedback);
+            } else if (key.Equals(SURVEY_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.Feedback);
+            } else if (key.Equals(STAR_RATING_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.StarRating);
+            } else if (key.Equals(PUSH_ACTION_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.Push);
+            } else if (key.Equals(ORIENTATION_EVENT_KEY)) {
+                return IsConsentGiven(ConsentFeatures.Users);
+            } else {
+                return IsConsentGiven(ConsentFeatures.Events);
+            }
+
         }
 
         /// <summary>
@@ -1025,8 +1044,10 @@ namespace CountlySDK.CountlyCommon
         internal bool consentRequired = false;
         internal Dictionary<ConsentFeatures, bool> givenConsent = new Dictionary<ConsentFeatures, bool>();
 
-        public enum ConsentFeatures { Sessions, Events, Location, Crashes, Users, Views };
-
+        public enum ConsentFeatures
+        {
+            Sessions, Events, Location, Crashes, Users, Views, Push, Feedback, StarRating, RemoteConfig
+        };
         internal bool IsConsentGiven(ConsentFeatures feature)
         {
             Debug.Assert(givenConsent != null);
@@ -1115,10 +1136,15 @@ namespace CountlySDK.CountlyCommon
 
 
         //track views
-        private String lastView = null;
+        private string lastView = null;
         private long lastViewStart = 0;
         private bool firstView = true;
-        private const String VIEW_EVENT_KEY = "[CLY]_view";
+        private const string NPS_EVENT_KEY = "[CLY]_nps";
+        private const string VIEW_EVENT_KEY = "[CLY]_view";
+        private const string SURVEY_EVENT_KEY = "[CLY]_survey";
+        private const string STAR_RATING_EVENT_KEY = "[CLY]_star_rating";
+        private const string PUSH_ACTION_EVENT_KEY = "[CLY]_push_action";
+        private const string ORIENTATION_EVENT_KEY = "[CLY]_orientation";
 
         /// <summary>
         /// Records view
@@ -1150,7 +1176,7 @@ namespace CountlySDK.CountlyCommon
                 firstView = false;
                 segm.Add("start", "1");
             }
-            return await RecordEventInternal(VIEW_EVENT_KEY, 1, null, null, segm, true);
+            return await RecordEventInternal(VIEW_EVENT_KEY, 1, null, null, segm);
         }
 
         /// <summary>
@@ -1177,7 +1203,7 @@ namespace CountlySDK.CountlyCommon
                 segm.Add("dur", "" + timestampSeconds);
                 segm.Add("segment", "Windows");
 
-                await RecordEventInternal(VIEW_EVENT_KEY, 1, null, null, segm, true);
+                await RecordEventInternal(VIEW_EVENT_KEY, 1, null, null, segm);
 
                 lastView = null;
                 lastViewStart = 0;
