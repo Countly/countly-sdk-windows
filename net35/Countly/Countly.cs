9a66522fc8b01d96e895d968d2f0a98ddcf30339
ZahidZafar <zahidzafar51@gmail.com>
2022-02-25 17:31:22 +0500

    Code cleanup and formating

--- net35/Countly/Countly.cs
+++ net35/Countly/Countly.cs
@@ -128,7 +128,7 @@ namespace CountlySDK
             SessionTimerStart();
 
             TimeInstant timeInstant = timeHelper.GetUniqueInstant();
-            Metrics metrics = new Metrics(DeviceData.OS, DeviceData.OSVersion, DeviceData.DeviceName, DeviceData.Resolution, null, AppVersion, DeviceData.Locale);            
+            Metrics metrics = new Metrics(DeviceData.OS, DeviceData.OSVersion, DeviceData.DeviceName, DeviceData.Resolution, null, AppVersion, DeviceData.Locale);
             await AddSessionEvent(new BeginSession(AppKey, await DeviceData.GetDeviceId(), sdkVersion, metrics, sdkName(), timeInstant));
         }
 
