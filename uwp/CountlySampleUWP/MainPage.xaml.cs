30f528f563f0b72ec51928d0af904f5b59b5417e
ZAHID-ZAFAR\zahid <zahidzafar51@gmail.com>
2022-05-16 17:04:38 +0500

    uwp sample app updated

--- uwp/CountlySampleUWP/MainPage.xaml.cs
+++ uwp/CountlySampleUWP/MainPage.xaml.cs
@@ -29,44 +29,62 @@ namespace CountlySampleUWP
             this.InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void Record_Event(object sender, RoutedEventArgs e)
         {
-            //Record Handled Crash
-            try
-            {
-                throw new DivideByZeroException();
+
+            await Countly.RecordEvent("Basic Event");
+        }
+
+        private async void Record_Event_1(object sender, RoutedEventArgs e)
+        {
+            Segmentation segmentation = new Segmentation();
+            segmentation.Add("retry", "3");
+            segmentation.Add("time_left", "60");
+
+            await Countly.RecordEvent("Basic Event", 1, 10, 10.5, segmentation);
+        }
+
+        private async void Record_View(object sender, RoutedEventArgs e)
+        {
+            await Countly.Instance.RecordView("Start View");
+        }
+
+        private async void Record_Exception(object sender, RoutedEventArgs e)
+        {
+            try {
+                throw new Exception("Exception with segmentation");
+            } catch (Exception ex) {
+                Dictionary<string, string> customInfo = new Dictionary<string, string>();
+                customInfo.Add("customData", "importantStuff");
+
+                await Countly.RecordException(ex.Message, ex.StackTrace, customInfo);
             }
-            catch (Exception ex)
-            {
-                await Countly.RecordException(ex.Message, ex.StackTrace);
-            }
         }
 
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        private async void Change_DeviceID_With_Merge(object sender, RoutedEventArgs e)
         {
-            //Record View
-            await Countly.Instance.RecordView("Some View Name");
+            await Countly.Instance.ChangeDeviceId("new-device-id-1", true);
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private async void Change_DeviceID_Without_Merge(object sender, RoutedEventArgs e)
         {
-            //throwing unhandled exception
-            throw new IndexOutOfRangeException();
+            await Countly.Instance.ChangeDeviceId("new-device-id-1");
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void Set_User_Profile(object sender, RoutedEventArgs e)
         {
-            bool consent = true;
+            Countly.UserDetails.Name = "full name";
+            Countly.UserDetails.Username = "username1";
+            Countly.UserDetails.Email = "test@count.ly";
+            Countly.UserDetails.Organization = "organization";
+            Countly.UserDetails.Phone = "000-111-000 ";
+            Countly.UserDetails.Gender = "Male";
+        }
 
-            Countly.Instance.SetConsent(new Dictionary<ConsentFeatures, bool>
-            {
-                { ConsentFeatures.Crashes, consent },
-                { ConsentFeatures.Events, consent },
-                { ConsentFeatures.Location, false },
-                { ConsentFeatures.Sessions, consent },
-                { ConsentFeatures.Users, false },
-                { ConsentFeatures.Views, consent }
-            });
+        private async void Set_User_Location(object sender, RoutedEventArgs e)
+        {
+            await Countly.Instance.SetLocation("31.5204, 74.3587", "192.0.0.1", "PK", "Lahore");
+
         }
     }
 }
