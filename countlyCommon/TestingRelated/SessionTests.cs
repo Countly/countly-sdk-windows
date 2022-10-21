acb0f52ecb48c9004eca47aa8049cc3429cb98ed
Zahid Zafar <zahidzafar51@gmail.com>
2022-10-21 16:39:21 +0500

    sesson with location unit tests added

--- countlyCommon/TestingRelated/SessionTests.cs
+++ countlyCommon/TestingRelated/SessionTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Web;
 using CountlySDK;
 using CountlySDK.CountlyCommon.Entities;
 using CountlySDK.Entities;
 using Xunit;
+using static CountlySDK.CountlyCommon.CountlyBase;
 
 namespace TestProject_common
 {
@@ -62,6 +64,94 @@ namespace TestProject_common
             NameValueCollection collection = HttpUtility.ParseQueryString(model.Request.Substring(2));
 
             Assert.Equal("1", collection.Get("begin_session"));
+            Assert.False(string.IsNullOrEmpty(collection.Get("metrics")));
+            ValidateSessionRequestParams(collection, "YOUR_APP_KEY", "device-id", "0");
+        }
+
+        [Fact]
+        /// <summary>
+        /// It validates 'SessionBegin' method with location values.
+        /// </summary>
+        public async void TestSessionBeginWithLocation()
+        {
+            CountlyConfig cc = new CountlyConfig {
+                serverUrl = "https://try.count.ly",
+                appKey = "YOUR_APP_KEY",
+                developerProvidedDeviceId = "device-id"
+            };
+            cc.SetLocation("PK", "Lahore", "0,0", "192.0.0.0.1");
+
+            await Countly.Instance.Init(cc);
+            await Countly.Instance.SessionBegin();
+
+            StoredRequest model = Countly.Instance.StoredRequests.Dequeue();
+            NameValueCollection collection = HttpUtility.ParseQueryString(model.Request.Substring(2));
+
+            Assert.Equal("1", collection.Get("begin_session"));
+
+            Assert.Equal("PK", collection.Get("country_code"));
+            Assert.Equal("Lahore", collection.Get("city"));
+            Assert.Equal("0,0", collection.Get("location"));
+            Assert.Equal("192.0.0.0.1", collection.Get("ip_address"));
+
+            Assert.False(string.IsNullOrEmpty(collection.Get("metrics")));
+            ValidateSessionRequestParams(collection, "YOUR_APP_KEY", "device-id", "0");
+        }
+
+        [Fact]
+        /// <summary>
+        /// It validates 'SessionBegin' method with location disable.
+        /// </summary>
+        public async void TestSessionBeginWithLocationDisable()
+        {
+            CountlyConfig cc = new CountlyConfig {
+                serverUrl = "https://try.count.ly",
+                appKey = "YOUR_APP_KEY",
+                developerProvidedDeviceId = "device-id"
+            };
+            cc.DisableLocation();
+
+            await Countly.Instance.Init(cc);
+            await Countly.Instance.SessionBegin();
+
+            StoredRequest model = Countly.Instance.StoredRequests.Dequeue();
+            NameValueCollection collection = HttpUtility.ParseQueryString(model.Request.Substring(2));
+
+            Assert.Equal("1", collection.Get("begin_session"));
+            Assert.Equal(string.Empty, collection.Get("location"));
+
+            Assert.False(string.IsNullOrEmpty(collection.Get("metrics")));
+            ValidateSessionRequestParams(collection, "YOUR_APP_KEY", "device-id", "0");
+        }
+
+        [Fact]
+        /// <summary>
+        /// It validates 'SessionBegin' method with no location consent.
+        /// </summary>
+        public async void TestSessionBeginWithoutLocationConsent()
+        {
+            CountlyConfig cc = new CountlyConfig {
+                serverUrl = "https://try.count.ly",
+                appKey = "YOUR_APP_KEY",
+                developerProvidedDeviceId = "device-id",
+                consentRequired = true
+            };
+
+            Dictionary<ConsentFeatures, bool> consent = new Dictionary<ConsentFeatures, bool>();
+            consent.Add(ConsentFeatures.Location, false);
+            consent.Add(ConsentFeatures.Sessions, true);
+
+            await Countly.Instance.Init(cc);
+            Countly.Instance.StoredRequests.Clear();
+
+            await Countly.Instance.SessionBegin();
+
+            StoredRequest model = Countly.Instance.StoredRequests.Dequeue();
+            NameValueCollection collection = HttpUtility.ParseQueryString(model.Request.Substring(2));
+
+            Assert.Equal("1", collection.Get("begin_session"));
+            Assert.Equal(string.Empty, collection.Get("location"));
+
             Assert.False(string.IsNullOrEmpty(collection.Get("metrics")));
             ValidateSessionRequestParams(collection, "YOUR_APP_KEY", "device-id", "0");
         }
