c77c7512aec7102b0c8259d190ca65fba7a60e0d
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2024-01-26 09:32:40 +0300

    Update Global.asax.cs

--- net45/CountlySampleAspNetMVC/Global.asax.cs
+++ net45/CountlySampleAspNetMVC/Global.asax.cs
@@ -10,12 +10,13 @@ namespace CountlySampleAspNetMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        public const string serverURL = "https://your.server.ly";
-        public const string appKey = "YOUR_APP_KEY";
+        private const string serverURL = "https://your.server.ly";
+        private const string appKey = "YOUR_APP_KEY";
+        
         protected async void Application_Start()
         {
             if (serverURL.Equals("https://your.server.ly") || appKey.Equals("YOUR_APP_KEY")) {
-                throw new Exception("Please do not use default set of app key and server url");
+                Debug.WriteLine("Please do not use default set of app key and server url");
             }
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
